bddc3797e38bb0d23ec66c2e362bd22b2836d391
Thai Pangsakulyanont <dtinth@spacet.me>
2022-02-17 16:55:07 +0000

    Remove loading spinner using `useDeferredValue`

--- src/pages/message/[mid].js
+++ src/pages/message/[mid].js
@@ -1,24 +1,27 @@
-import { Suspense } from "react";
+import { Suspense, useDeferredValue } from "react";
 import { useRouter } from "next/router";
 import useSWR from "swr";
 import Spinner from "../../components/spinner";
 
 export default function Message() {
   let router = useRouter();
+  let targetMid = router.query.mid;
+  let displayedMid = useDeferredValue(targetMid);
+  let loading = targetMid !== displayedMid;
   return (
     <div className="w-full p-8 overflow-y-scroll">
       <Suspense fallback={<Spinner />}>
-        <MessageView mid={router.query.mid} />
+        <MessageView mid={displayedMid} loading={loading} />
       </Suspense>
     </div>
   );
 }
 
-function MessageView({ mid }) {
+function MessageView({ mid, loading }) {
   let { data } = useSWR(`/api/messages/${mid}`, { suspense: true });
 
   return (
-    <>
+    <div style={{ opacity: loading ? 0.5 : 1 }}>
       <h1 className="text-2xl font-bold">{data.message.title}</h1>
 
       <div className="mt-6 space-y-2 text-zinc-400">
@@ -26,6 +29,6 @@ function MessageView({ mid }) {
           <p key={i}>{paragraph}</p>
         ))}
       </div>
-    </>
+    </div>
   );
 }
